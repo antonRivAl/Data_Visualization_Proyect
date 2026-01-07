77496c7ed00ebda5e2cad6517342983131122306
pauhermig02 <paulaherrero02@hotmail.com>
2026-01-07 09:05:20 +0100

    Cambio en packages

--- packages.R
+++ packages.R
@@ -4,4 +4,4 @@ install.packages("dplyr")
 install.packages("readr")      
 install.packages("tidyr") 
 install.packages("ggplot2")
-install.packages("leaflet") # Obliga a instalar primero las dependecias que te pida en el SO, por lo menos a mi
+install.packages("leaflet")
